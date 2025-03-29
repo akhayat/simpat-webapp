fe32bebdc31c40cb35be50b0fede71a259876978
Anthony Khayat <anthonykhayat1@gmail.com>
2025-03-28 17:14:22 -0700

    Added docker support and switched to fetch from jquery ajax

--- src/App.js
+++ src/App.js
@@ -1,27 +1,33 @@
 import './App.css';
-import { useState } from 'react'
-import $ from 'jquery';
+import { useState, useEffect } from 'react'
 
 function App() {
   const [table, setTable] = useState(<p>loading...</p>)
 
-  $.getJSON("http://157.230.203.35:8000/table-info", function (data) {
-    setTable(
-    <table className="DataTable">
-      <thead>
-        <tr key={"header"}>
-          {Object.keys(data[0]).map((prop) => (<th key={prop}>{prop}</th>))}
-        </tr>
-      </thead>
-      <tbody>
-        {data.map((item) => (
-          <tr key={item.TableName}>
-            {Object.values(item).map((val, index) => (<td key={index}>{val}</td>))}
-          </tr>
-        ))}
-      </tbody>
-    </table>)
-  });
+  useEffect(() => {
+    fetch(process.env.REACT_APP_DATA_SERVER_URL + "/table-info")
+      .then((response) => response.json())
+      .then((data) => {
+        setTable(
+          <table className="DataTable">
+            <thead>
+              <tr key={"header"}>
+                {Object.keys(data[0]).map((prop) => (<th key={prop}>{prop}</th>))}
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((item) => (
+                <tr key={item.TableName}>
+                  {Object.values(item).map((val, index) => (<td key={index}>{val}</td>))}
+                </tr>
+              ))}
+            </tbody>
+          </table>)
+      })
+      .catch(() => {
+        setTable(<p>An error occurred while fetching data</p>);
+      });
+  }, []);
 
   return <div className="Container">{table}</div>
 }
